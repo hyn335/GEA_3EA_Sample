84c79627a6a2c2ca871a5ebc702fbb385de5eba0
hyn335 <hyn335@naver.com>
2025-09-30 15:49:26 +0900

    9/30 적 ai 생성

--- My project (1)/Assets/Script/Enemy.cs
+++ My project (1)/Assets/Script/Enemy.cs
@@ -1,26 +1,114 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public enum EnemyState { Idle, Trace, Attack }
+
+    public EnemyState state = EnemyState.Idle;
+
     public float moveSpeed = 2f; //이동 속도
 
+    public float traceRange = 15f; //추적 시간 거리 
+
+    public float attackRange = 6f; // 공격 시작 거리 
+
+    public float attackCooldown = 1.5f;
+
+    public GameObject projectileprefab; // 투사체 프리팹
+
+    public Transform firePoint;   //발사 위치
+
     private Transform player;    //플레이어 추적용
+
+    private float lastAttackTime;
+
+    public int maxHP = 5;
+
+    private int currentHP;
+  
+    
+    
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        lastAttackTime = -attackCooldown;
+        currentHP = maxHP;
     }
+
+
+
 
     // Update is called once per frame
     void Update()
     {
         if (player == null) return;
 
-        //플레이어까지의 방향 구하기 
-        Vector3 direction = (player.position - transform.position).normalized;
-        transform.position += direction * moveSpeed * Time.deltaTime;
+        float dist = Vector3.Distance(player.position, transform.position);
+
+        // FSM 상태 전환 
+        switch(state)
+        {
+            case EnemyState.Idle:
+                if (dist < traceRange)
+                    state = EnemyState.Trace;
+                break;
+
+            case EnemyState.Trace:
+                if (dist < attackRange)
+                    state = EnemyState.Attack;
+                else if (dist > traceRange)
+                    state = EnemyState.Idle;
+                else
+                    TracePlayer();
+                break;
+
+            case EnemyState.Attack:
+                if (dist > attackRange)
+                    state = EnemyState.Trace;
+                else
+                    AttackPlayer();
+                break;
+                
+        }
+    }
+    
+   
+    void TracePlayer()
+    {
+        Vector3 dir = (player.position - transform.position).normalized;
+        transform.position += dir * moveSpeed * Time.deltaTime;
         transform.LookAt(player.position);
     }
+    
+    void AttackPlayer()
+    {
+        //일정 쿨다운마다 발사 
+        if (Time.time >= lastAttackTime + attackCooldown)
+        {
+            lastAttackTime = Time.time;
+            Shootprojectile();
+        }
+    }
+    
+    void Shootprojectile()
+    {
+        if (projectileprefab != null && firePoint != null)
+        {
+            transform.LookAt(player.position);
+            GameObject proj = Instantiate(projectileprefab, firePoint.position, firePoint.rotation);
+            EnemyProjectile ep = proj.GetComponent<EnemyProjectile>();
+            if (ep != null)
+            {
+                Vector3 dir = (player.position - firePoint.position).normalized;
+                ep.SetDirection(dir);
+            }
+
+        }
+    }
+
 }
--- My project (1)/Assets/Script/PlayerController.cs
+++ My project (1)/Assets/Script/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,16 +11,31 @@ public class PlayerController : MonoBehaviour
 
     public float gravity = -9.81f;
 
+    public CinemachineVirtualCamera virtualCam;
+
+    public float rotationSpeed = 10f;
+    private CinemachinePOV pov;
+
     private CharacterController controller;
 
     private Vector3 velocity;
 
     public bool isGrounded;
 
+    public int maxHP = 100;
+
+    private int currentHP;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        pov = virtualCam.GetCinemachineComponent<CinemachinePOV>();
+        //virtual camera의 POV 컴포넌트 가져오기
+
+        currentHP = maxHP;
     }
 
     // Update is called once per frame
@@ -39,5 +55,18 @@ public class PlayerController : MonoBehaviour
         }
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
+    public void TakeDamege(int damage)
+    {
+        currentHP -= damage;
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
     }
 }
