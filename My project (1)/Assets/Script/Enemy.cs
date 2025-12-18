37491d15843d335727a7bacbe9fd3ec3e54c4258
hyn335 <hyn335@naver.com>
2025-12-19 00:35:06 +0900

    12/19 enemy 코드 삭제

--- My project (1)/Assets/Script/Enemy.cs
+++ My project (1)/Assets/Script/Enemy.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
